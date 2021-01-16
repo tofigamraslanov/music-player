ae110d38021a5eac6df7738218478205fa95e55b
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-01-17 00:48:51 +0400

    Music Player App using C# Windows Forms:)

--- MusicPlayerApp/Form1.Designer.cs
+++ MusicPlayerApp/Form1.Designer.cs
@@ -38,6 +38,7 @@ namespace MusicPlayerApp
             this.ClientSize = new System.Drawing.Size(412, 218);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
