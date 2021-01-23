ecabd6f0eeb9e03f6f32e4f99d10f87f3325e0f8
Tofig Amraslanov <tofiqamraslanov@gmail.com>
2021-01-23 12:57:19 +0400

    App is completed

--- MusicPlayerApp/Form1.Designer.cs
+++ MusicPlayerApp/Form1.Designer.cs
@@ -52,13 +52,13 @@ namespace MusicPlayerApp
             this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelHeader.Location = new System.Drawing.Point(0, 0);
             this.panelHeader.Name = "panelHeader";
-            this.panelHeader.Size = new System.Drawing.Size(638, 44);
+            this.panelHeader.Size = new System.Drawing.Size(786, 44);
             this.panelHeader.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(581, 8);
+            this.pictureBox1.Location = new System.Drawing.Point(731, 8);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(26, 26);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -78,13 +78,14 @@ namespace MusicPlayerApp
             // 
             // Songs
             // 
-            this.Songs.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Songs.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Songs.FormattingEnabled = true;
-            this.Songs.ItemHeight = 20;
-            this.Songs.Location = new System.Drawing.Point(431, 62);
+            this.Songs.ItemHeight = 18;
+            this.Songs.Location = new System.Drawing.Point(500, 62);
             this.Songs.Name = "Songs";
-            this.Songs.Size = new System.Drawing.Size(176, 244);
+            this.Songs.Size = new System.Drawing.Size(257, 292);
             this.Songs.TabIndex = 1;
+            this.Songs.SelectedIndexChanged += new System.EventHandler(this.Songs_SelectedIndexChanged);
             // 
             // btnSelectSongs
             // 
@@ -92,12 +93,13 @@ namespace MusicPlayerApp
             this.btnSelectSongs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSelectSongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSelectSongs.ForeColor = System.Drawing.Color.White;
-            this.btnSelectSongs.Location = new System.Drawing.Point(431, 312);
+            this.btnSelectSongs.Location = new System.Drawing.Point(500, 365);
             this.btnSelectSongs.Name = "btnSelectSongs";
-            this.btnSelectSongs.Size = new System.Drawing.Size(176, 38);
+            this.btnSelectSongs.Size = new System.Drawing.Size(257, 38);
             this.btnSelectSongs.TabIndex = 2;
             this.btnSelectSongs.Text = "Select Songs";
             this.btnSelectSongs.UseVisualStyleBackColor = false;
+            this.btnSelectSongs.Click += new System.EventHandler(this.btnSelectSongs_Click);
             // 
             // WindowsMediaPlayerMusic
             // 
@@ -105,7 +107,7 @@ namespace MusicPlayerApp
             this.WindowsMediaPlayerMusic.Location = new System.Drawing.Point(32, 62);
             this.WindowsMediaPlayerMusic.Name = "WindowsMediaPlayerMusic";
             this.WindowsMediaPlayerMusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("WindowsMediaPlayerMusic.OcxState")));
-            this.WindowsMediaPlayerMusic.Size = new System.Drawing.Size(384, 288);
+            this.WindowsMediaPlayerMusic.Size = new System.Drawing.Size(462, 341);
             this.WindowsMediaPlayerMusic.TabIndex = 3;
             // 
             // panelFooter
@@ -115,15 +117,15 @@ namespace MusicPlayerApp
             this.panelFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panelFooter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panelFooter.ForeColor = System.Drawing.Color.YellowGreen;
-            this.panelFooter.Location = new System.Drawing.Point(0, 381);
+            this.panelFooter.Location = new System.Drawing.Point(0, 436);
             this.panelFooter.Name = "panelFooter";
-            this.panelFooter.Size = new System.Drawing.Size(638, 41);
+            this.panelFooter.Size = new System.Drawing.Size(786, 41);
             this.panelFooter.TabIndex = 4;
             // 
             // lblFooter
             // 
             this.lblFooter.AutoSize = true;
-            this.lblFooter.Location = new System.Drawing.Point(188, 12);
+            this.lblFooter.Location = new System.Drawing.Point(234, 12);
             this.lblFooter.Name = "lblFooter";
             this.lblFooter.Size = new System.Drawing.Size(280, 20);
             this.lblFooter.TabIndex = 0;
@@ -135,7 +137,7 @@ namespace MusicPlayerApp
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(638, 422);
+            this.ClientSize = new System.Drawing.Size(786, 477);
             this.Controls.Add(this.panelFooter);
             this.Controls.Add(this.WindowsMediaPlayerMusic);
             this.Controls.Add(this.btnSelectSongs);
--- MusicPlayerApp/Form1.cs
+++ MusicPlayerApp/Form1.cs
@@ -21,6 +21,32 @@ namespace MusicPlayerApp
         {
 
         }
+        // Creating global variables of string type array to save the name of the tracks and path of the tracks
+        string[] path, files;
+
+        private void btnSelectSongs_Click(object sender, EventArgs e)
+        {
+            // Code to select songs
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                files = openFileDialog.SafeFileNames;
+                path = openFileDialog.FileNames;
+
+                // Dislay the music name in listbox
+                foreach (var file in files)
+                {
+                    Songs.Items.Add(file);
+                }
+            }
+        }
+
+        private void Songs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Code to play music
+            WindowsMediaPlayerMusic.URL = path[Songs.SelectedIndex];
+        }
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
